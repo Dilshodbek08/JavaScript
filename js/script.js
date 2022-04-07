a4e0ce4da4a69a78fe60d93e51a021adbce3dd2c
Dilshodbek08 <devel0perdilsh0dbek@gmail.com>
2022-04-07 11:21:02 +0500

    Java Script

---
+++ js/script.js
@@ -0,0 +1,19 @@
+alert("Assalomu aleykum. Saytimizga xush kelibsiz!!!");
+var firstName = prompt("Ismingiz nima?");
+var lastName = prompt("Familiyangiz nima?");
+var age = prompt("Yoshingiz nechida?");
+var job = prompt("Qerda ishlisiz(yoki o'qiysiz)?");
+var level = prompt("Darajangiz(junior,middle,senior)?");
+var salary = prompt("Maoshingiz qancha?");
+var thanks = prompt("Thanks for giving information😉");
+
+console.log(
+  "Ismingiz: " + firstName + "\n" +
+  "Familiyangiz: " + lastName + "\n" +
+  "Yoshingiz: " + age + "\n" +
+  "Ishingiz: " + job + "\n" +
+  "Darajangiz: " + level + "\n" +
+  "Maoshingiz: " + salary + "\n" +
+  "Sizga kattakon raxmat, siz haqizda hamma ma'lumotni bilvoldim😉: " + thanks
+);
+
